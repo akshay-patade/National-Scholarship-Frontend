b44ed12652c31a477ce1d066d939f0d5790c5d33
mdnabi <mdnabi506@gmail.com>
2019-02-11 17:05:41 +0530

    binding remaining of student registration form

--- src/app/Student/student-registration.component.ts
+++ src/app/Student/student-registration.component.ts
@@ -1,4 +1,5 @@
 import {  Component } from '@angular/core'
+import { Student } from './student';
 
 
 @Component({
@@ -7,6 +8,8 @@ import {  Component } from '@angular/core'
 
 })
 export class StudentRegistrationComponent {
-
+    students: Student=new Student();
+    
+    
 
 }  
